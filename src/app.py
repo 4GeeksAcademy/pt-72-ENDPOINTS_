1e9822b6242a0494d65d31c9c50e9a2427ce11ab
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2024-11-04 23:49:29 +0000

    Made some endpoints

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Favorites, Clients, Leads
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,76 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/users', methods=['GET'])
 def handle_hello():
+    all_users = User.query.all()
+    users = list(map(lambda x: x.serialize(), all_users))
+    return jsonify(users), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/users/<int:id>/favorites', methods=['GET'])
+def get_user_fav(id):
+    all_favorites = Favorites.query.filter_by(user_id = id)
+    fav = list(map(lambda x: x.serialize(), all_favorites))
+    return jsonify(fav), 200
 
-    return jsonify(response_body), 200
+@app.route('/users/<int:user_id>/favorites/client/<int:client_id>', methods=['POST'])
+def post_favorite_client(user_id, client_id):
+    favorite = Favorites(user_id = user_id, client_id = client_id, lead_id= "NULL")
+    db.session.add(favorite)
+    db.session.commit()
+    return jsonify(favorite.serialize()), 200
+
+@app.route('/users/<int:user_id>/favorites/lead/<int:lead>', methods=['POST'])
+def post_favorite_lead(user_id, lead_id):
+    favorite = Favorites(user_id = user_id, client_id = "NULL", lead_id= lead_id)
+    db.session.add(favorite)
+    db.session.commit()
+    return jsonify(favorite.serialize()), 200
+
+@app.route('/users/<int:user_id>/favorites/client/<int:client_id>', methods=['DELETE'])
+def delete_favorite_client(user_id, client_id):
+    client = Favorites.query.filter_by(user_id = user_id, client_id = client_id).first()
+    db.session.delete(client)
+    db.session.commit()
+    return jsonify("You deleted a favorite client")
+
+@app.route('/users/<int:user_id>/favorites/lead/<int:lead_id>', methods=['DELETE'])
+def delete_favorite_lead(user_id, lead_id):
+    lead = Favorites.query.filter_by(user_id = user_id, lead_id = lead_id).first()
+    db.session.delete(lead)
+    db.session.commit()
+    return jsonify("You deleted a favorite lead")
+
+@app.route('/clients', methods=['GET'])
+def get_all_clients():
+    all_clients = Clients.query.all()
+    clients = list(map(lambda x: x.serialize(), all_clients))
+    return jsonify(clients), 200
+
+@app.route('/clients/<int:client_id>', methods=['GET'])
+def get_each_clients(client_id):
+    client = Clients.query.filter_by(id = client_id).first()
+    return jsonify(client.serialize())
+
+@app.route('/leads', methods=['GET'])
+def get_all_leads():
+    all_leads = Leads.query.all()
+    leads = list(map(lambda x: x.serialize(), all_leads))
+    return jsonify(leads), 200
+
+@app.route('/leads/<int:lead_id>', methods=['GET'])
+def get_each_lead(lead_id):
+    lead = Leads.query.filter_by(id = lead_id).first()
+    return jsonify(lead.serialize())
+
+@app.route('/clients', methods=['POST'])
+def post_clients():
+    data = request.get_json()
+    client = Clients(name = data['name'], email = data['email'])
+    db.session.add(client)
+    db.session.commit()
+    return jsonify(client.serialize()), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -7,13 +7,49 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorites')
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    client_id = db.Column(db.Integer, db.ForeignKey('clients.id'))
+    lead_id = db.Column(db.Integer, db.ForeignKey('leads.id'))
+
+    
+class Clients(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    favorites = db.relationship('Favorites')
+  
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    
+class Leads(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    favorites = db.relationship('Favorites')
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
